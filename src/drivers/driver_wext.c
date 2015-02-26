a87c456e59a6256660c117b245f4a62044d11a5c
George Talusan <george.talusan@gmail.com>
2015-02-26 10:43:17 -0500

    netronix bogus SSID removal

--- src/drivers/driver_wext.c
+++ src/drivers/driver_wext.c
@@ -1731,19 +1731,6 @@ static void wpa_driver_wext_disconnect(struct wpa_driver_wext_data *drv)
 			}
 			return;
 		}
-		/*
-		 * Clear the BSSID selection and set a random SSID to make sure
-		 * the driver will not be trying to associate with something
-		 * even if it does not understand SIOCSIWMLME commands (or
-		 * tries to associate automatically after deauth/disassoc).
-		 */
-		for (i = 0; i < 32; i++)
-			ssid[i] = rand() & 0xFF;
-		if (wpa_driver_wext_set_bssid(drv, null_bssid) < 0 ||
-		    wpa_driver_wext_set_ssid(drv, ssid, 32) < 0) {
-			wpa_printf(MSG_DEBUG, "WEXT: Failed to set bogus "
-				   "BSSID/SSID to disconnect");
-		}
 	}
 }
 
